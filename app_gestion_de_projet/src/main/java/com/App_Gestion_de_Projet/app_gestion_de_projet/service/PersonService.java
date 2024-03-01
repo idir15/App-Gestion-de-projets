784b6c08e317320706899b3c85616fb1fefef71d
idir15 <nechab.idir15@gmail.com>
2024-03-01 15:34:05 +0100

    delete Project

--- app_gestion_de_projet/src/main/java/com/App_Gestion_de_Projet/app_gestion_de_projet/service/PersonService.java
+++ app_gestion_de_projet/src/main/java/com/App_Gestion_de_Projet/app_gestion_de_projet/service/PersonService.java
@@ -1,9 +1,7 @@
 package com.App_Gestion_de_Projet.app_gestion_de_projet.service;
 
 import com.App_Gestion_de_Projet.app_gestion_de_projet.model.Person;
-import com.App_Gestion_de_Projet.app_gestion_de_projet.model.Project;
 import com.App_Gestion_de_Projet.app_gestion_de_projet.repository.PersonRepository;
-import com.App_Gestion_de_Projet.app_gestion_de_projet.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
