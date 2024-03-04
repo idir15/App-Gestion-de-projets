41570e2a7b6395cb66cba9d6849407da0f8ceef9
idir15 <nechab.idir15@gmail.com>
2024-03-04 10:33:38 +0100

    add method delete

--- app_gestion_de_projet/src/main/java/com/App_Gestion_de_Projet/app_gestion_de_projet/service/TaskService.java
+++ app_gestion_de_projet/src/main/java/com/App_Gestion_de_Projet/app_gestion_de_projet/service/TaskService.java
@@ -3,11 +3,14 @@ package com.App_Gestion_de_Projet.app_gestion_de_projet.service;
 import com.App_Gestion_de_Projet.app_gestion_de_projet.repository.TaskRepository;
 import com.App_Gestion_de_Projet.app_gestion_de_projet.model.Task;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.HashMap;
 import java.util.List;
-
-
-
+import java.util.Map;
 
 
 @Service
@@ -29,6 +32,14 @@ public class TaskService {
 
     public Task saveTask(Task task) {
         return taskRepository.save(task);
+    }
+
+
+    public ResponseEntity<Map<String, String>> deleteTask(Long id) {
+        taskRepository.deleteById(id);
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Task deleted successfully");
+        return ResponseEntity.ok(response);
     }
 
 }
